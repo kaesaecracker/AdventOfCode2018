3743d5ca38f3dd01a0dad3b2e86b499209ac905e
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-12-02 13:19:02 +0100

    Part II: Double Values

---
+++ Day1/DoubleValues/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DoubleValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("First parameter should be input file");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.Error.WriteLine($"File '{args[0]}' does not exist");
+                return;
+            }
+
+            long sum = 0;
+            var reachedSums = new HashSet<long>();
+            while (true)
+                using (var tr = new StreamReader(args[0]))
+                    while (!tr.EndOfStream)
+                    {
+                        var line = tr.ReadLine();
+                        sum += int.Parse(line);
+
+                        if (reachedSums.Contains(sum))
+                        {
+                            Console.WriteLine($"Double value: {sum}");
+                            return;
+                        }
+
+                        reachedSums.Add(sum);
+                    }
+        }
+    }
+}
