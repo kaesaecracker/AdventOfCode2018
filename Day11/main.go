635fcb61a83c197af76223f6e78122b4b60e4247
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-12-27 17:22:35 +0100

    Day 11: Add some comments

--- Day11/main.go
+++ Day11/main.go
@@ -5,15 +5,15 @@ import (
 	"math"
 )
 
-type empty struct{}
-type semaphore chan empty
-
 const gridSerialNumber = 9445 // this is the input
 const gridSize = 300
 
+// fuel levels for each cell in grid
 var fuelGrid = [gridSize][gridSize]int{}
+// combined levels for [x][y][size]
 var combinedLevels = [gridSize][gridSize][gridSize]int{}
 
+// returns the fuel level of the cell
 func calcFuelLevel(x int, y int) int {
 	rackId := x + 10
 	level := rackId * y
@@ -25,6 +25,7 @@ func calcFuelLevel(x int, y int) int {
 	return level
 }
 
+// returns the hundreds place of the number (0 <= x <= 9) or 0 if there is none
 func getHundreds(i int) int {
 	if i < 100 {
 		return 0
@@ -33,6 +34,7 @@ func getHundreds(i int) int {
 	return i / 100 % 10
 }
 
+// Given the top left corner and the size of the square it returns the sum of the levels. This does *not* check the indices accessed, so it has to be called carefully.
 func getCombinedFuelLevel(x int, y int, size int) int {
 	sum := 0
 
@@ -45,20 +47,29 @@ func getCombinedFuelLevel(x int, y int, size int) int {
 	return sum
 }
 
-func main() {
-	// calc fuel levels for grid
+// calculate the fuel levels for every cell in the grid
+func populateFuelGrid() {
 	for x := 1; x <= gridSize; x++ {
 		for y := 1; y <= gridSize; y++ {
 			fuelGrid[x-1][y-1] = calcFuelLevel(x, y)
 		}
 	}
+}
 
-	// calculate fuel levels (one goroutine for each x,y)
+// calculate fuel levels (one goroutine for each x,y)
+func populateCombinedLevels() {
+	// semaphore pattern
+	type empty struct{}
+	type semaphore chan empty
 	sem := make(semaphore, gridSize^2)
+
+	// for each cell
 	for x := 1; x <= gridSize; x++ {
 		for y := 1; y <= gridSize; y++ {
 
+			// asynchronously call anonymous func that calculates the levels for possible sizes
 			go func(x int, y int) {
+				//             make sure (x+size | y+size) is in array
 				for size := 1; (x+size < gridSize) && (y+size < gridSize); size++ {
 					combinedLevels[x-1][y-1][size-1] = getCombinedFuelLevel(x, y, size)
 				}
@@ -73,9 +84,17 @@ func main() {
 	for i := 0; i < gridSize^2; i++ {
 		<-sem
 	}
+}
 
+func main() {
+	populateFuelGrid()
+	populateCombinedLevels()
+
+	// vars to remember solution values
 	threeX, threeY, threeLevel := -1, -1, math.MinInt32
 	biggestX, biggestY, biggestSize, biggestLevel := -1, -1, -1, math.MinInt32
+
+	// find solution values in calculated levels
 	for x := 1; x <= gridSize; x++ {
 		for y := 1; y <= gridSize; y++ {
 			for size := 1; size <= gridSize; size++ {
@@ -93,6 +112,7 @@ func main() {
 		}
 	}
 
+	// output (duh)
 	fmt.Printf("Part I:  %d,%d \t\t= %d\n", threeX, threeY, threeLevel)
 	fmt.Printf("Part II: %d,%d,%d \t= %d\n", biggestX, biggestY, biggestSize, biggestLevel)
 }
