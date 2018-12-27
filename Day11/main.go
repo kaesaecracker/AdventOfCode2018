97a464ccc3ecea11b5618784fa15c337d6594dfd
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-12-27 15:57:17 +0100

    Part II (used goroutines for the first time in my life)

--- Day11/main.go
+++ Day11/main.go
@@ -5,10 +5,14 @@ import (
 	"math"
 )
 
+type empty struct{}
+type semaphore chan empty
+
 const gridSerialNumber = 9445 // this is the input
 const gridSize = 300
 
 var fuelGrid = [gridSize][gridSize]int{}
+var combinedLevels = [gridSize][gridSize][gridSize]int{}
 
 func calcFuelLevel(x int, y int) int {
 	rackId := x + 10
@@ -29,11 +33,11 @@ func getHundreds(i int) int {
 	return i / 100 % 10
 }
 
-func getCombinedFuelLevel(x int, y int) int {
+func getCombinedFuelLevel(x int, y int, size int) int {
 	sum := 0
 
-	for offX := 0; offX < 3; offX++ {
-		for offY := 0; offY < 3; offY++ {
+	for offX := 0; offX < size; offX++ {
+		for offY := 0; offY < size; offY++ {
 			sum += fuelGrid[x+offX-1][y+offY-1]
 		}
 	}
@@ -42,25 +46,53 @@ func getCombinedFuelLevel(x int, y int) int {
 }
 
 func main() {
+	// calc fuel levels for grid
 	for x := 1; x <= gridSize; x++ {
 		for y := 1; y <= gridSize; y++ {
 			fuelGrid[x-1][y-1] = calcFuelLevel(x, y)
 		}
 	}
 
-	biggestX := -1
-	biggestY := -1
-	biggestLevel := math.MinInt32
-	for x := 1; x <= gridSize-2; x++ {
-		for y := 1; y <= gridSize-2; y++ {
-			combLevel := getCombinedFuelLevel(x, y)
-			if combLevel > biggestLevel {
-				biggestLevel = combLevel
-				biggestX = x
-				biggestY = y
+	// calculate fuel levels (one goroutine for each x,y)
+	sem := make(semaphore, gridSize^2)
+	for x := 1; x <= gridSize; x++ {
+		for y := 1; y <= gridSize; y++ {
+
+			go func(x int, y int) {
+				for size := 1; (x+size < gridSize) && (y+size < gridSize); size++ {
+					combinedLevels[x-1][y-1][size-1] = getCombinedFuelLevel(x, y, size)
+				}
+
+				sem <- empty{}
+			}(x, y)
+
+		}
+	}
+
+	// wait for the goroutines to finish
+	for i := 0; i < gridSize^2; i++ {
+		<-sem
+	}
+
+	threeX, threeY, threeLevel := -1, -1, math.MinInt32
+	biggestX, biggestY, biggestSize, biggestLevel := -1, -1, -1, math.MinInt32
+	for x := 1; x <= gridSize; x++ {
+		for y := 1; y <= gridSize; y++ {
+			for size := 1; size <= gridSize; size++ {
+				level := combinedLevels[x-1][y-1][size-1]
+
+				if size == 3 && threeLevel < level {
+					threeX, threeY, threeLevel = x, y, level
+				}
+
+				if biggestLevel < level {
+					biggestX, biggestY, biggestSize, biggestLevel = x, y, size, level
+				}
+
 			}
 		}
 	}
 
-	fmt.Printf("Solution: %d,%d = %d\n", biggestX, biggestY, biggestLevel)
+	fmt.Printf("Part I:  %d,%d \t\t= %d\n", threeX, threeY, threeLevel)
+	fmt.Printf("Part II: %d,%d,%d \t= %d\n", biggestX, biggestY, biggestSize, biggestLevel)
 }
